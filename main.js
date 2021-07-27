0a11b4f15241db9823ad5e53fdeff727c65dd69a
1 <tawers6103@gmail.com>
2021-07-27 16:03:35 +0300

    js

--- main.js
+++ main.js
@@ -3,30 +3,34 @@
 // https://jsonplaceholder.typicode.com/users
 //     кожному елементу юзера створити кнопку, при клику на яку в окремий блок виводяться всі пости поточного юзера.
 //     Кожному елементу post створити кнопку, при клику на яку в окремий блок виводяться всі коментарі поточного поста
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(value => value.json())
-    .then(posts => {
 
-        for (const post of posts) {
-            let userDiv = document.createElement('div');
-            let btn = document.createElement('button');
-            userDiv.append(btn);
-            btn.innerText = 'press'
-            let h2 = document.createElement('h2')
-            let h3 = document.createElement('h3')
-            let p = document.createElement('p')
-            let span = document.createElement('span')
-            btn.onclick = function (){
-                //userDiv.append(`${post.id}`,`${post.userId}`,`${post.title}`,`${post.body}`)
-                userDiv.append(h2)
-                userDiv.append(h3)
+
+fetch('https://jsonplaceholder.typicode.com/users')
+     .then(value => value.json())
+     .then(users => {
+         for (let user of users) {
+         let userDiv = document.createElement('div');
+                let btn = document.createElement('button');
+                let block = document.createElement('div')
+
+                let p = document.createElement('p')
                 userDiv.append(p)
-                userDiv.append(span)
-                h2.innerText = `${post.id}`
-                h3.innerText = `${post.userId}`
-                p.innerText = `${post.title}`
-                span.innerText = `${post.body}`
-            }
-            let target = document.getElementById('target');
-            target.append(userDiv);
-        }})
+                userDiv.append(block)
+                userDiv.append(btn);
+                btn.innerText = 'press'
+                p.innerText = `${user.id} ${user.name} ${user.username}`
+                btn.onclick = function () {
+                    fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
+                        .then(value => value.json())
+                        .then(posts => {
+                            block.innerHTML = ''
+                            for (const post of posts) {
+                                let li = document.createElement('li')
+                                block.appendChild(li)
+                                li.innerText = `${post.title}`
+                            }
+                        })
+                }
+                let target = document.getElementById('target');
+                target.append(userDiv);
+         }})
